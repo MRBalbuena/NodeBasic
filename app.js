f8104b7af83e156ece2914432ba5d594a010389a
mrbalbuena <mrbalbuena@hotmail.com>
2016-11-14 16:46:01 +0000

    Complete navigation, splitting views and routes. Pass navigation into route as param

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
-var bookRouter = express.Router();
+//var bookRouter = express.Router(); // now is in another file
+var nav = [{
+            Link: '/Books',
+            Text: 'Books'
+        },{
+            Link: '/Authors',
+            Text: 'Authors'
+        }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public')); // first looks here
 app.use(express.static('src/views')); // then here
@@ -10,61 +18,12 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
-var books = [{
-    title: 'C#',
-    genre: 'Server side progarmming1',
-    author: 'Marcelo'
-},{
-    title: 'Javascript#',
-    genre: 'Client Programming',
-    author: 'Marcelo'
-},{
-    title: 'SQL Server',
-    genre: 'Databases Programming',
-    author: 'Marcelo'
-},{
-    title: 'CSS',
-    genre: 'Client Programming',
-    author: 'Marcelo'
-},{
-    title: 'Angular2',
-    genre: 'Client Programming',
-    author: 'Marcelo'
-}
-];
-
-bookRouter.route('/')
-    .get(function(req, res){
-        res.render('books', {
-        title: 'Books', 
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'
-        },{
-            Link: '/Authors',
-            Text: 'Authors'
-        }],
-        books: books
-    });
-});
-
-bookRouter.route('/single')
-    .get(function(req, res){
-        res.send('Hello Single Book');
-    });
-
 app.use('/Books', bookRouter);
 
 app.get('/', function(req, res){
     res.render('index', {
         title: 'Hello from render', 
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'
-        },{
-            Link: '/Authors',
-            Text: 'Authors'
-        }]
+        nav: nav
     });
 });
 
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var bookRouter = express.Router();
+
+var router = function (nav) {
+
+    var books = [{
+        title: 'C#',
+        genre: 'Server side progarmming1',
+        author: 'Marcelo'
+    }, {
+        title: 'Javascript#',
+        genre: 'Client Programming',
+        author: 'Marcelo'
+    }, {
+        title: 'SQL Server',
+        genre: 'Databases Programming',
+        author: 'Marcelo'
+    }, {
+        title: 'CSS',
+        genre: 'Client Programming',
+        author: 'Marcelo'
+    }, {
+        title: 'Angular2',
+        genre: 'Client Programming',
+        author: 'Marcelo'
+    }
+    ];
+
+    bookRouter.route('/')
+        .get(function (req, res) {
+            res.render('bookListView', {
+                title: 'Books',
+                nav: nav,
+                books: books
+            });
+        });
+
+    // this is replaced by :id to pass a single book id {*1}
+    // bookRouter.route('/single')
+    //     .get(function(req, res){
+    //         res.send('Hello Single Book');
+    //     });
+
+    //{*1}
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = req.params.id;
+            res.render('bookView', {
+                title: 'Books',
+                nav: nav,
+                book: books[id]
+            });
+        });
+    return bookRouter
+};
+
+module.exports = router;
