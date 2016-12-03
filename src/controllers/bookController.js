fbe61fe1cb7f52ced36980178210b67b7a0ab12b
mrbalbuena <mrbalbuena@hotmail.com>
2016-12-03 13:03:38 +0000

    split in controllers to make code better

---
+++ src/controllers/bookController.js
@@ -0,0 +1,51 @@
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectId;
+
+var bookController = function (bookService, nav) {
+    var middleware = function (req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    };
+    var getIndex = function (req, res) {
+        var url = 'mongodb://localhost:27017/libraryApp';
+        mongodb.connect(url, function (err, db) {
+            if (err) { console.log(err); }
+            var collection = db.collection('books');
+            collection.find({}).toArray(function (err, results) {
+                if (err) { res.send(err); }
+                console.log(results);
+                res.render('bookListView', {
+                    title: 'Book',
+                    nav: nav,
+                    books: results
+                });
+                db.close();
+            });
+        });
+    };
+    var getById = function (req, res) {
+        var id = new objectId(req.params.id);
+        var url = 'mongodb://localhost:27017/libraryApp';
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('books');
+            collection.findOne({ _id: id }, function (err, results) {
+                if (err) { console.log(err); }
+                res.render('bookView', {
+                    title: 'Books',
+                    nav: nav,
+                    book: results
+                });
+            });
+        });
+    };
+
+    return {
+        getIndex: getIndex,
+        getById: getById,
+        middleware: middleware
+    }
+}
+
+module.exports = bookController;
--- src/routes/bookRoutes_no_controllers.js
+++ src/routes/bookRoutes_no_controllers.js
@@ -1,3 +1,4 @@
+// this version implements everything here
 var express = require('express');
 var bookRouter = express.Router();
 //var sql = require('mssql');
@@ -5,13 +6,31 @@ var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectId;
 
 var router = function (nav) {
-    var bookController = require('../controllers/bookController')(null, nav);
     //console.log('ok');
-    bookRouter.use(bookController.middleware);
+    bookRouter.use(function (req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    });
     bookRouter.route('/')
-        .get(bookController.getIndex);
-    bookRouter.route('/:id')
-        .get(bookController.getById);
+        .get(function (req, res) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                if (err) { console.log(err); }
+                var collection = db.collection('books');
+                collection.find({}).toArray(function (err, results) {
+                    if (err) { res.send(err); }
+                    console.log(results);
+                    res.render('bookListView', {
+                        title: 'Book',
+                        nav: nav,
+                        books: results
+                    });
+                    db.close();
+                });
+            });
+        });
     // connect and get data from SQL Server
     // var request = new sql.Request();
     // request.query('select * from books', function (err, recordset) {
@@ -61,6 +80,22 @@ var router = function (nav) {
     //     });
 
     // mongodb
+    bookRouter.route('/:id')
+        .get(function (req, res, next) {
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.findOne({ _id: id }, function (err, results) {
+                    if (err) { console.log(err); }
+                    res.render('bookView', {
+                        title: 'Books',
+                        nav: nav,
+                        book: results
+                    });
+                });
+            });
+        });
 
     return bookRouter;
 };
